a375e543d36a484ac9aaf644e3c8e6ea0a505d5e
AbelHutten <a.stormhutten@gmail.com>
2025-03-02 20:30:38 +0100

    Partial implementation of GQA and README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # grouped-query-attention-pytorch
+
 Grouped Query Attention implementation in PyTorch
+
+From the paper: https://arxiv.org/abs/2305.13245
+
---
+++ src.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class GroupedQueryAttention(nn.Module):
+    """Grouped Query Attention as described in https://arxiv.org/abs/2305.13245."""
+
+    def __init__(self):
+        # TODO: determine what parameters I want to take
+        # TODO: implement init
+        pass
+
+    def forward():
+        # TODO: Implement the forward function
+        pass
+
+
+# TODO: write some tests and benchmarks
+
+nn.MultiheadAttention
